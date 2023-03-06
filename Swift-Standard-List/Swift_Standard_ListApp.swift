9b007686570be854c85819b61cf03eeb7c8afbcb
Anais <anaiszed447@gmail.com>
2023-03-05 19:35:34 -0500

    Initial Commit

---
+++ Swift-Standard-List/Swift_Standard_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Standard_ListApp.swift
+//  Swift-Standard-List
+//
+//  Created by Anais Zulfequar on 2023-03-05.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Standard_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
